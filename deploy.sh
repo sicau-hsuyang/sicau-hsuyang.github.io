fe3b005a7ab87b922f196cc9bc57c65555044fdd
“yangxu” <404189928@qq.com>
2022-07-26 22:57:07 +0800

    demo

--- deploy.sh
+++ deploy.sh
@@ -4,20 +4,20 @@
 set -e
 
 # 生成静态文件
-# npm run docs:build
+npm run docs:build
 
 # 进入生成的文件夹
-# cd docs/.vuepress/dist
+cd docs/.vuepress/dist
 
 # 如果是发布到自定义域名
 # echo 'www.example.com' > CNAME
 
-# git init
-# git add -A
-# git commit -m 'deploy'
+git init
+git add -A
+git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:sicau-hsuyang/sicau-hsuyang.github.io.git deploy
+git push -f git@github.com:sicau-hsuyang/sicau-hsuyang.github.io.git deploy
 
 
 cd -
