5868833df65057f4bdf7d476200545663d9c81f5
“yangxu” <404189928@qq.com>
2022-07-26 22:17:03 +0800

    modify

--- deploy.sh
+++ deploy.sh
@@ -17,7 +17,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:sicau-hsuyang/sicau-hsuyang.github.io.git master
+git push -f git@github.com:sicau-hsuyang/sicau-hsuyang.github.io.git deplay
 
 
 cd -
