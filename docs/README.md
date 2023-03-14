21ff2735cbe50bf1211d8db6980228e87bbfcd0f
“yangxu” <404189928@qq.com>
2023-03-14 17:47:16 +0800

    feat: 🎸 更新README

--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,18 @@
 亲爱的读者，你好，我是 `杨旭`，英文名 `JohnYang`，邮箱`404189928@qq.com`欢迎来到 `awesome-frontend-code` 。
 
-短短一年时间，我经历了两次裁员，可能现在正在浏览本文的朋友你也受到了一定的影响吧！
+去年短短一年时间，我经历了两次裁员，于是我开始思考如何在这种互联网寒冬下提高自己的竞争力，想到将自己这几年的所学归类整理，编写成博客，希望能帮助前进路上的你。
 
-于是，我开始思考如何在这种互联网寒冬下提高自己的竞争力，想到将自己这几年的所学归类整理，编写成博客，希望能帮助前进路上的你。
+现在互联网上到处都充斥着前端已死的氛围，但是我并不这样认为，虽然这几年受疫情的影响，全球经济都遭受了重创，IT 行业也无法独善其身。
 
-目前这个仓库还在不断的更新中。
+社会能提供的岗位肯定比以前更少了（而且还有培训机构不断的在制造竞争者），但是在这样的大环境下，你只能比别人更愿意卷才能先拔头筹，别怕，你只需要静下心好好学习，时间会给你答案的。
 
-如果读者对我的能力认可的话，欢迎帮我内推，目前主要考虑**成都前端**的岗位。
+**宝剑锋从磨砺出，梅花香自苦寒来~**
+
+目前这个仓库还在不断的更新中...
 
 更新顺序为数据结构和算法专题->设计模式专题->JavaScript 专题->前端框架专题->杂项专题（主要会涵盖`http`,`webpack`, `typescript`）
 
-当前更新节为数据结构和算法章节。
+近期不会再更新数据结构和算法章节。
 
 ## 算法和数据结构专题
 
@@ -28,18 +30,24 @@
 
 ## 设计模式专题
 
-待更新...
+- [单例模式](https://sicau-hsuyang.github.io/design-pattern/singleton.html)
+- [代理模式](https://sicau-hsuyang.github.io/design-pattern/proxy.html)
+- [观察者模式](https://sicau-hsuyang.github.io/design-pattern/watcher.html)
+- [状态模式](https://sicau-hsuyang.github.io/design-pattern/state.html)
+
+更新中...
 
 ## JavaScript 专题
 
-- [函数上下文this](https://sicau-hsuyang.github.io/javascript/this.html)
+- [函数上下文 this](https://sicau-hsuyang.github.io/javascript/this.html)
 - [原型及原型链](https://sicau-hsuyang.github.io/javascript/prototype.html)
 - [继承](https://sicau-hsuyang.github.io/javascript/extend.html)
 - [闭包](https://sicau-hsuyang.github.io/javascript/closure.html)
-- [手写JS常见的API](https://sicau-hsuyang.github.io/javascript/write/clone.html)
+- [手写 JS 常见的 API](https://sicau-hsuyang.github.io/javascript/write/clone.html)
 - [ES6](https://sicau-hsuyang.github.io/javascript/es6/class.html)
 
 更新中...
+
 ## 前端框架专题
 
 待更新...
---
+++ docs/design-pattern/command.ts
@@ -0,0 +1,177 @@
+// interface Command {
+//   action: Action;
+
+//   exec(): void;
+// }
+
+// class CopyCommand implements Command {
+//   constructor(public action: Action) {}
+
+//   exec(): void {
+//     this.action.work();
+//   }
+// }
+
+// class Invoker {
+//   protected cmd: Command;
+
+//   setCommand(cmd: Command) {
+//     this.cmd = cmd;
+//   }
+
+//   execCommand() {
+//     this.cmd.exec();
+//   }
+// }
+
+// class Action {
+//   work() {
+//     console.log("干活儿");
+//   }
+// }
+
+// const copyAction = new Action();
+
+// const copyCmd = new CopyCommand(copyAction);
+
+// const invoker = new Invoker();
+
+// invoker.setCommand(copyCmd);
+
+// invoker.execCommand();
+
+// abstract class Command {
+//   abstract exec(): void;
+// }
+
+// class CopyCommand extends Command {
+//   exec(): void {
+//     console.log("执行copy");
+//   }
+// }
+
+// class Invoker {
+//   command: Command;
+
+//   setCommand(cmd: Command) {
+//     this.command = cmd;
+//   }
+
+//   execCommand() {
+//     this.command.exec();
+//   }
+// }
+
+abstract class Command {
+  protected preVal: number;
+  abstract exec(prev: number, ...args: number[]): number;
+  abstract cancel(): number;
+}
+
+class AddCommand extends Command {
+  protected preVal: number = 0;
+
+  exec(prev: number, ...args: number[]): number {
+    this.preVal = prev;
+    return args.reduce((prev, cur) => {
+      return prev + cur;
+    }, prev);
+  }
+
+  cancel(): number {
+    return this.preVal;
+  }
+}
+
+class MultiCommand extends Command {
+  protected preVal: number = 0;
+
+  exec(prev: number, ...args: number[]): number {
+    return args.reduce((prev, cur) => {
+      return prev - cur;
+    }, prev);
+  }
+
+  cancel(): number {
+    return this.preVal;
+  }
+}
+
+class MinusCommand extends Command {
+  prevVal = 0;
+
+  exec(prev: number, ...args: number[]): number {
+    this.prevVal = prev;
+    return args.reduce((prev, cur) => {
+      return prev * cur;
+    }, prev);
+  }
+
+  cancel(): number {
+    return this.prevVal;
+  }
+}
+
+class Calculator {
+  maxLength = 10;
+
+  historyQueue: Command[] = [];
+
+  addCmd: Command;
+
+  minusCmd: Command;
+
+  multiCmd: Command;
+
+  setAddCmd(cmd: Command) {
+    this.addCmd = cmd;
+  }
+
+  sum = 0;
+
+  done(action: string, ...num: number[]) {
+    switch (action) {
+      case "+":
+        this.add(num);
+        break;
+      case "-":
+        this.minus(num);
+      case "*":
+        this.multi(num);
+        break;
+    }
+  }
+
+  undone() {
+    const node = this.historyQueue.pop();
+    if (!node) {
+      console.log("没有可以撤销的命令啦~");
+      return;
+    }
+    const cmd = node;
+    this.sum = cmd.cancel();
+  }
+
+  recordCommand(cmd: Command) {
+    this.historyQueue.push(cmd);
+    if (this.historyQueue.length > this.maxLength) {
+      console.log("超出了最大的记录数，系统将丢弃一些记录");
+      this.historyQueue.shift();
+    }
+  }
+
+  add(nums: number[]) {
+    this.sum = this.addCmd.exec(this.sum, ...nums);
+    this.recordCommand(this.addCmd);
+  }
+
+  minus(nums: number[]) {
+    this.sum = this.minusCmd.exec(this.sum, ...nums);
+    this.recordCommand(this.minusCmd);
+  }
+
+  multi(nums: number[]) {
+    this.sum = this.multiCmd.exec(this.sum, ...nums);
+    this.recordCommand(this.multiCmd);
+  }
+}
