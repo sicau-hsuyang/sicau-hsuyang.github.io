15bffba85d0e95581a95bf8f5b581c2157addfc6
“yangxu” <404189928@qq.com>
2023-03-20 22:21:36 +0800

    fix: 🐛 移除结点参数调用错误

--- docs/javascript/write/jsonp.js
+++ docs/javascript/write/jsonp.js
@@ -1,17 +1,17 @@
 function jsonp(url, data) {
   return new Promise((resolve, reject) => {
-    const randomMethodName = (Math.random() * 10 ** 10).toFixed(0);
+    const randomMethodName = "jsonp" + (Math.random() * 10 ** 10).toFixed(0);
     const script = document.createElement("script");
     let queryString = "?";
     for (let prop in data) {
       queryString += `&${prop}=${JSON.stringify(data[prop])}&`;
     }
-    url += queryString.replace(/^&*|&*$/g, '');
+    url += queryString.replace(/^&*|&*$/g, "");
     url += "&jsonpCallback=" + randomMethodName;
     script.src = url;
     script.type = "text/javascript";
     script.onload = function success() {
-      document.body.parentElement.removeChild(script);
+      document.body.removeChild(script);
     };
     script.onerror = reject;
     window[randomMethodName] = function (...args) {
