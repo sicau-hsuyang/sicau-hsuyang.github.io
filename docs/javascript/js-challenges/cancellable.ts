544bcb50f40b8c539e16a6dddd379e2d2bf31f3f
“yangxu” <404189928@qq.com>
2023-04-28 23:00:37 +0800

    refactor: 💡 继续重构代码

--- docs/javascript/js-challenges/cancellable.ts
+++ docs/javascript/js-challenges/cancellable.ts
@@ -1,3 +1,39 @@
+type Action = "next" | "throw";
+
+export function cancellable<T>(
+  generator: Generator<Promise<any>, T, unknown>
+): [() => void, Promise<T>] {
+  let cancel: () => void = () => {};
+
+  const p: Promise<T> = new Promise((resolve, reject) => {
+    const forward = (val: any, type: Action = "next") => {
+      try {
+        const step = generator[type](val);
+        if (step.done) {
+          resolve(step.value);
+          return;
+        }
+        Promise.resolve(step.value)
+          .then(forward)
+          .catch((error) => {
+            forward(error, "throw");
+          });
+      } catch (exp) {
+        reject(exp);
+      }
+    };
+
+    cancel = () => {
+      forward("Cancelled", "throw");
+    };
+
+    forward(undefined);
+  });
+  return [cancel, p];
+}
+
+
+/**
 export function cancellable<T>(
   generator: Generator<Promise<any>, T, unknown>
 ): [() => void, Promise<T>] {
@@ -33,3 +69,4 @@ export function cancellable<T>(
   });
   return [cancel, p];
 }
+ */
