0d3d674bbba0aa485e50d740b2e8bac1ad950e15
“yangxu” <404189928@qq.com>
2023-05-01 16:05:57 +0800

    feat: 🎸 增加数字转中文大写,中文大写转数字

--- docs/javascript/snippets/money-transfer.spec.ts
+++ docs/javascript/snippets/money-transfer.spec.ts
@@ -1,4 +1,4 @@
-import { digitToChinese } from "./money-transfer";
+import { digitToChinese, chineseNumCaseToDigit } from "./money-transfer";
 
 describe("money transfer unit test", () => {
   it("0", () => {
@@ -50,6 +50,11 @@ describe("money transfer unit test", () => {
   it("100000000", () => {
     const tenMillion = digitToChinese(100000000);
     expect(tenMillion).toBe("壹亿圆整");
+  });
+
+  it("-100000000", () => {
+    const tenMillion = digitToChinese(-100000000);
+    expect(tenMillion).toBe("负壹亿圆整");
   });
 
   it("100000001", () => {
@@ -120,5 +125,146 @@ describe("money transfer unit test", () => {
   it("1000003452", () => {
     const bigNum = digitToChinese(1000003452);
     expect(bigNum).toBe("壹拾亿零叁仟肆佰伍拾贰圆整");
+  });
+});
+
+describe("money case transfer to number unit test", () => {
+  it("零圆整", () => {
+    const zero = chineseNumCaseToDigit("零圆整");
+    expect(zero).toBe(0);
+  });
+
+  it("壹拾圆整", () => {
+    const ten = chineseNumCaseToDigit("壹拾圆整");
+    expect(ten).toBe(10);
+  });
+
+  it("壹佰圆整", () => {
+    const hundred = chineseNumCaseToDigit("壹佰圆整");
+    expect(hundred).toBe(100);
+  });
+
+  it("壹佰贰拾叁圆肆角伍分", () => {
+    const result = 123.45;
+    const res = chineseNumCaseToDigit("壹佰贰拾叁圆肆角伍分");
+    expect(res).toBe(result);
+  });
+
+  it("壹仟圆整", () => {
+    const thousand = chineseNumCaseToDigit("壹仟圆整");
+    expect(thousand).toBe(1000);
+  });
+
+  it("壹萬圆整", () => {
+    const tenThousand = chineseNumCaseToDigit("壹萬圆整");
+    expect(tenThousand).toBe(10000);
+  });
+
+  it("壹拾萬圆整", () => {
+    const hundredThousand = chineseNumCaseToDigit("壹拾萬圆整");
+    expect(hundredThousand).toBe(100000);
+  });
+
+  it("壹佰萬圆整", () => {
+    const million = chineseNumCaseToDigit("壹佰萬圆整");
+    expect(million).toBe(1000000);
+  });
+
+  it("壹仟萬圆整", () => {
+    const tenMillion = chineseNumCaseToDigit("壹仟萬圆整");
+    expect(tenMillion).toBe(10000000);
+  });
+
+  it("壹亿圆整", () => {
+    const tenMillion = chineseNumCaseToDigit("壹亿圆整");
+    expect(tenMillion).toBe(100000000);
+  });
+
+  it("负壹亿圆整", () => {
+    const tenMillion = chineseNumCaseToDigit("负壹亿圆整");
+    expect(tenMillion).toBe(-100000000);
+  });
+
+  it("壹亿零壹圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹亿零壹圆整");
+    expect(bigNum).toBe(100000001);
+  });
+
+  it("壹拾亿零壹拾壹圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹拾亿零壹拾壹圆整");
+    expect(bigNum).toBe(1000000011);
+  });
+
+  it("壹佰亿零壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹佰亿零壹佰壹拾贰圆整");
+    expect(bigNum).toBe(10000000112);
+  });
+
+  it("壹拾亿零贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹拾亿零贰仟壹佰壹拾贰圆整");
+    expect(bigNum).toBe(1000002112);
+  });
+
+  it("壹拾亿零肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹拾亿零肆萬贰仟壹佰壹拾贰圆整");
+    expect(bigNum).toBe(1000042112);
+  });
+
+  it("壹拾亿零叁拾肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹拾亿零叁拾肆萬贰仟壹佰壹拾贰圆整");
+    expect(bigNum).toBe(1000342112);
+  });
+
+  it("壹拾亿零捌佰叁拾肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "壹拾亿零捌佰叁拾肆萬贰仟壹佰壹拾贰圆整"
+    );
+    expect(bigNum).toBe(1008342112);
+  });
+
+  it("壹拾亿零贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "壹拾亿零贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整"
+    );
+    expect(bigNum).toBe(1028342112);
+  });
+
+  it("壹拾壹亿贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "壹拾壹亿贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整"
+    );
+    expect(bigNum).toBe(1128342112);
+  });
+
+  it("壹仟壹佰壹拾壹亿贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "壹仟壹佰壹拾壹亿贰仟捌佰叁拾肆萬贰仟壹佰壹拾贰圆整"
+    );
+    expect(bigNum).toBe(111128342112);
+  });
+
+  it("壹佰贰拾叁萬肆仟伍佰陆拾柒圆捌角玖分", () => {
+    const bigNum =
+      chineseNumCaseToDigit("壹佰贰拾叁萬肆仟伍佰陆拾柒圆捌角玖分");
+    expect(bigNum).toBe(1234567.89);
+  });
+
+  it("玖亿玖仟玖佰玖拾玖萬玖仟玖佰玖拾玖圆玖角玖分", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "玖亿玖仟玖佰玖拾玖萬玖仟玖佰玖拾玖圆玖角玖分"
+    );
+    expect(bigNum).toBe(999999999.99);
+  });
+
+  it("玖亿玖仟玖佰玖拾玖萬玖仟玖佰玖拾玖圆零玖分", () => {
+    const bigNum = chineseNumCaseToDigit(
+      "玖亿玖仟玖佰玖拾玖萬玖仟玖佰玖拾玖圆零玖分"
+    );
+    expect(bigNum).toBe(999999999.09);
+  });
+
+  it("壹拾亿零叁仟肆佰伍拾贰圆整", () => {
+    const bigNum = chineseNumCaseToDigit("壹拾亿零叁仟肆佰伍拾贰圆整");
+    expect(bigNum).toBe(1000003452);
   });
 });
--- docs/javascript/snippets/money-transfer.ts
+++ docs/javascript/snippets/money-transfer.ts
@@ -5,6 +5,7 @@
  */
 export function digitToChinese(num: number) {
   let chinese: string = "";
+  // 定义中文的单位映射
   const upperCaseUnit = [
     "零",
     "壹",
@@ -17,21 +18,28 @@ export function digitToChinese(num: number) {
     "捌",
     "玖",
   ];
+  // 较大的单位
   const bigUnit = ["萬", "亿"];
+  // 进制单位
   const transUnits = ["", "拾", "佰", "仟"];
-  const numDigits: string[] = String(num).split(".")[0].split("").reverse();
+  const digitGroup = String(Math.abs(num)).split(".");
+  // 将数字转为字符串数组，并将其反转，让低位数字排在数组的前面
+  const numDigits: string[] = digitGroup[0].split("").reverse();
   let hasAddedZero = false;
   for (let i = 0; i < numDigits.length; i++) {
     const digit = numDigits[i];
     let chineseDigit = upperCaseUnit[digit];
+    // 处理大的单位
     let unit: string = "";
     if (i % 4 === 0 && i > 0) {
       unit = bigUnit[i / 4 - 1];
     }
+    // 遇到数字为0的时候，在没有添加过的情况下，添加0️，因为不可能出现连续的两个0
     if (chineseDigit === "零" && !hasAddedZero) {
       chinese = "零" + chinese;
       hasAddedZero = true;
     } else if (chineseDigit != "零") {
+      // 遇到非零的数字之后，将添加0的标记清除
       hasAddedZero = false;
       unit += transUnits[i % 4];
       chinese = chineseDigit + unit + chinese;
@@ -43,23 +51,102 @@ export function digitToChinese(num: number) {
       unit = "";
     }
   }
+  // 如果最后一位是0，并且用户输入的不是0的话，那最后一个零需要去掉
   if (chinese != "零") {
     chinese = chinese.replace(/零$/g, "");
   }
+  // 对于整数亿，在上述的处理过程中会出现连续的`亿万`，将万字去掉
   chinese = chinese.replace(/亿萬/, "亿");
+  // 追加单位
   chinese += "圆";
-  // 处理小数
+  // 处理小数，不考虑.00这种情况
   if (Math.floor(num) != num) {
     const fragUnits = ["角", "分"];
     // 取两位
-    const fragNumbers = String(num).split(".")[1].split("").slice(0, 2);
+    const fragNumbers = digitGroup[1].split("").slice(0, 2);
     for (let i = 0; i < fragNumbers.length; i++) {
       const chineseDigit = upperCaseUnit[fragNumbers[i]];
       const chineseUnit = fragUnits[i];
+      // 小数，也需要考虑0的情况
       chinese += chineseDigit + (chineseDigit != "零" ? chineseUnit : "");
     }
   } else {
     chinese += "整";
   }
+  if (num < 0) {
+    chinese = "负" + chinese;
+  }
   return chinese;
+}
+
+/**
+ * 中文大写字符串转数字
+ * @param chineseDigitCase 中文大写字符串
+ */
+export function chineseNumCaseToDigit(chineseDigitCase: string): number {
+  let sum: number = 0;
+  let negativeFlag = false;
+  if (/^负/.test(chineseDigitCase)) {
+    negativeFlag = true;
+    chineseDigitCase = chineseDigitCase.slice(1);
+  }
+  const yiIdx = chineseDigitCase.indexOf("亿");
+  if (yiIdx >= 0) {
+    const bigNum = smallTransform(chineseDigitCase.slice(0, yiIdx));
+    sum += bigNum * 100000000;
+    chineseDigitCase = chineseDigitCase.slice(yiIdx + 1);
+  }
+  const wanIdx = chineseDigitCase.indexOf("萬");
+  if (wanIdx >= 0) {
+    const bigNum = smallTransform(chineseDigitCase.slice(0, wanIdx));
+    sum += bigNum * 10000;
+    chineseDigitCase = chineseDigitCase.slice(wanIdx + 1);
+  }
+  const bigNum = smallTransform(chineseDigitCase);
+  sum += bigNum;
+  if (negativeFlag) {
+    sum *= -1;
+  }
+  return sum;
+}
+
+function smallTransform(chineseCase: string) {
+  let sum = 0;
+  const upperCaseUnit: Record<string, number> = {
+    零: 0,
+    壹: 1,
+    贰: 2,
+    叁: 3,
+    肆: 4,
+    伍: 5,
+    陆: 6,
+    柒: 7,
+    捌: 8,
+    玖: 9,
+  };
+  const thousand = chineseCase.match(/([壹贰叁肆伍陆柒捌玖])仟/);
+  if (Array.isArray(thousand)) {
+    sum += upperCaseUnit[thousand[1]] * 1000;
+  }
+  const hundred = chineseCase.match(/([壹贰叁肆伍陆柒捌玖])佰/);
+  if (Array.isArray(hundred)) {
+    sum += upperCaseUnit[hundred[1]] * 100;
+  }
+  const ten = chineseCase.match(/([壹贰叁肆伍陆柒捌玖])拾/);
+  if (Array.isArray(ten)) {
+    sum += upperCaseUnit[ten[1]] * 10;
+  }
+  const size = chineseCase.match(/([零壹贰叁肆伍陆柒捌玖])(?=$|圆)/);
+  if (Array.isArray(size)) {
+    sum += upperCaseUnit[size[1]];
+  }
+  const deciSomeone = chineseCase.match(/([壹贰叁肆伍陆柒捌玖])角/);
+  if (Array.isArray(deciSomeone)) {
+    sum += upperCaseUnit[deciSomeone[1]] * 0.1;
+  }
+  const centi = chineseCase.match(/([壹贰叁肆伍陆柒捌玖])分/);
+  if (Array.isArray(centi)) {
+    sum += upperCaseUnit[centi[1]] * 0.01;
+  }
+  return sum;
 }
