a4f7423ebcc0483a71459e2413057e5aedfab6aa
yangxu <yangxu01@changba.com>
2023-09-10 11:21:36 +0800

    fix: 🐛 先暂时注释代码

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,7 +20,7 @@ module.exports = {
         { title: "继承", path: "/javascript/extend" },
         { title: "闭包", path: "/javascript/closure" },
         { title: "Ajax", path: "/javascript/ajax" },
-        { title: "位运算", path: "/javascript/bit-operation" },
+        // { title: "位运算", path: "/javascript/bit-operation" },
         // {
         //   title: "JavaScript异步技术的发展进程",
         //   path: "/javascript/async-development-history",
